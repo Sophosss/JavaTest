86d472ed7c9e2a1b0346d037b31ea845a3a0e6db
“ee <1477261167@qq.com>
2019-07-31 15:17:19 +0800

    两个单链表相交的起始节点

---
+++ programming/src/leetcode/IntersectionOfTwoLists.java
@@ -0,0 +1,13 @@
+package leetcode;
+
+public class IntersectionOfTwoLists {
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		if(headA == null || headB == null) return null;
+		ListNode l1 = headA, l2 = headB;
+		while (l1 != l2){
+			l1 = l1 == null ? headB : l1.next;
+			l2 = l2 == null ? headA : l2.next;
+		}
+		return l1;
+	}
+}
