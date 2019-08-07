70510d628c9332f01e286f0b992b4846d54972b9
“ee <1477261167@qq.com>
2019-08-07 16:43:31 +0800

    合并区间

--- programming/src/leetcode/MergeIntervals.java
+++ programming/src/leetcode/MergeIntervals.java
@@ -1,8 +1,8 @@
 package leetcode;
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import	java.util.List;
+import java.util.List;
 
 public class MergeIntervals {
 	public int[][] merge(int[][] intervals) {
