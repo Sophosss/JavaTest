562a4163eb07d551ee144a06d819c5b26d9b9f84
“ee <1477261167@qq.com>
2019-08-08 11:21:29 +0800

    找到所有数组中消失的数字（复习）

---
+++ programming/src/leetcode/FindDisappearedNum.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDisappearedNum {
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> list = new ArrayList<>();
+		if (nums == null || nums.length == 0) return list;
+		for (int i = 0; i < nums.length;){
+			if (nums [i] == i + 1 || nums [i] == nums [nums[i] - 1]){
+				i++;
+			}else swap(nums, i, nums [i] - 1);
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums [i] != i + 1) list.add(i + 1);
+		}
+		return list;
+	}
+
+	public void swap(int [] nums, int i, int j){
+		nums [i] = nums [i] ^ nums [j];
+		nums [j] = nums [i] ^ nums [j];
+		nums [i] = nums [i] ^ nums [j];
+	}
+
+	public static void main(String[] args) {
+		int []nums = {4,3,2,7,8,2,3,1};
+		FindDisappearedNum findDisappearedNum = new FindDisappearedNum();
+		findDisappearedNum.findDisappearedNumbers(nums);
+	}
+}
