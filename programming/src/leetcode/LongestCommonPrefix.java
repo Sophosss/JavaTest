74c39eef0dc623322d3d85bd0cae959f21a8f1ff
“ee <1477261167@qq.com>
2019-08-09 15:31:21 +0800

    14.最长公共前缀

--- programming/src/leetcode/LongestCommonPrefix.java
+++ programming/src/leetcode/LongestCommonPrefix.java
@@ -2,15 +2,14 @@ package leetcode;
 
 public class LongestCommonPrefix {
 	public String longestCommonPrefix(String[] strs) {
-		if (strs == null || strs.length == 0) return null;
-		int n = strs.length - 1;
+		if (strs == null || strs.length == 0) return "";
 		int len = Integer.MAX_VALUE;
 		String ss = "";
 		for(String s : strs){
 			len = Math.min(len, s.length());
 		}
 		for (int i = 0; i < len; i++) {
-			for (int j = 0; j < n; j++) {
+			for (int j = 0; j < strs.length - 1; j++) {
 				if (strs[j].charAt(i) != strs[j+1].charAt(i)) return ss;
 			}
 			ss += strs[0].charAt(i);
