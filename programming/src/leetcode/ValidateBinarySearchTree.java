36add24db40f1d75643ebb643e40b99cc86a628f
“ee <1477261167@qq.com>
2019-08-11 14:34:50 +0800

    98.验证二叉搜索树(不推荐)

---
+++ programming/src/leetcode/ValidateBinarySearchTree.java
@@ -0,0 +1,25 @@
+package leetcode;
+import	java.util.HashMap.TreeNode;
+
+import java.util.ArrayList;
+
+public class ValidateBinarySearchTree {
+	public boolean isValidBST(TreeNode root) {
+		ArrayList<Integer> list = new ArrayList<>();
+		recur(list, root);
+		Integer[] integers = list.toArray(new Integer[0]);
+		for (int i = 0; i < integers.length - 1; i++) {
+			if (integers[i] > (integers[i + 1])) return false;
+		}
+		return true;
+	}
+
+	private void recur(ArrayList<Integer> list, TreeNode root) {
+	    if (root == null) return;
+	    else {
+	    	recur(list, root.left);
+	    	list.add(root.val);
+	    	recur(list, root.right);
+		}
+	}
+}
