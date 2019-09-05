a12dbdfe36b144ee53b6fb35ba0e280a5895e439
“ee <1477261167@qq.com>
2019-09-05 20:24:48 +0800

    2.两数相加

--- programming/src/leetcode/AddTwoNum.java
+++ programming/src/leetcode/AddTwoNum.java
@@ -1,32 +1,23 @@
 package leetcode;
 
 public class AddTwoNum {
-	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		long num1 = l1.val, num2 = l2.val,c1 = 0, c2 = 0,c3 = 0, c4 =0;
-		while (l1.next != null){
-			c3 += 1;
-			c4 = (long) Math.pow(10,c3);
-			num1 = l1.next.val * c4 +num1;
-			l1 = l1.next;
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+		ListNode dummy = new ListNode(0);
+		ListNode p = dummy;
+		int num1 = 0;
+		int temp =0;
+		while(l1 != null || l2 != null || num1 != 0){
+			temp = l1 == null ? 0 : l1.val;
+			temp += l2 == null ? 0 : l2.val;
+			temp += num1;
+			num1 = temp / 10;
+			ListNode node = new ListNode(temp % 10);
+			p.next = node;
+			p = node;
+			l1 = l1 == null ? l1 : l1.next;
+			l2 = l2 == null ? l2 : l2.next;
 		}
-		while (l2.next != null){
-			c2 += 1;
-			c1 = (long) Math.pow(10,c2);
-			num2 = l2.next.val * c1 +num2;
-			l2 = l2.next;
-		}
-		num1 = num1 + num2;
-		String s = num1 + "";
-		int n = s.length();
-		String s1 = Character.toString(s.charAt(0));
-		ListNode listNode = new ListNode(Integer.parseInt(s1));
-		for (int i = 1; i < n; i++) {
-			String s2 = Character.toString(s.charAt(i));
-			ListNode listNode1 = new ListNode(Integer.parseInt(s2));
-			listNode1.next = listNode;
-			listNode = listNode1;
-		}
-		return listNode;
+		return dummy.next;
 	}
 
 	public static void main(String[] args) {
@@ -58,7 +49,8 @@ public class AddTwoNum {
 		l1.next = l3;
 		l3.next = l4;
 
-		ListNode listNode = addTwoNumbers(l1,l2);
+		AddTwoNum addTwoNum = new AddTwoNum();
+		ListNode listNode = addTwoNum.addTwoNumbers(l1,l2);
 		while (listNode!=null){
 			System.out.println(listNode.val);
 			listNode = listNode.next;
