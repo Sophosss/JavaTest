e0375acaabea358a83a4191f5e1087676a83ad72
“ee <1477261167@qq.com>
2019-10-31 01:41:06 +0800

    92.反转链表II(头插法)

---
+++ programming/src/leetcode/ReverseLinkedListTwo.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class ReverseLinkedListTwo {
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode pre = dummy;
+		for(int i = 1; i < m; i++){
+			pre = pre.next;
+		}
+		head = pre.next;
+		for(int i = m; i < n; i++){
+			ListNode nex = head.next;
+			head.next = nex.next;
+			nex.next = pre.next;
+			pre.next = nex;
+		}
+		return dummy.next;
+	}
+}
