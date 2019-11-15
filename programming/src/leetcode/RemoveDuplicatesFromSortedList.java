2799f4b9d1269ef37c3d086bebc13fa3c3f9caee
“ee <1477261167@qq.com>
2019-11-15 19:31:29 +0800

    83.删除排序链表中的重复元素

---
+++ programming/src/leetcode/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,14 @@
+package leetcode;
+
+public class RemoveDuplicatesFromSortedList {
+	public ListNode deleteDuplicates(ListNode head) {
+		ListNode cur = head;
+		while(cur != null && cur.next != null){
+			if(cur.val == cur.next.val){
+				cur.next = cur.next.next;
+			}
+			else cur = cur.next;
+		}
+		return head;
+	}
+}
