158baa621a282f46b56359046a5d3188f4622f43
“ee <1477261167@qq.com>
2019-11-19 20:38:17 +0800

    146.LRU缓存机制(哈希链表)

--- programming/src/leetcode/LRUCache.java
+++ programming/src/leetcode/LRUCache.java
@@ -1,24 +1,54 @@
 package leetcode;
+import java.util.HashMap;
 import	java.util.LinkedHashMap;
 
 import java.util.Map;
-
+class Node{
+	Node pre, next;
+	int key, val;
+	public Node(int key, int val){
+		this.key = key;
+		this.val = val;
+	}
+}
 public class LRUCache {
-	private LinkedHashMap<Integer, Integer> map;
+	Node head;
+	Node tail;
+	Map<Integer, Node> map = new HashMap<>();
+	int cap;
 	public LRUCache(int capacity) {
-		map = new LinkedHashMap(capacity, 0.75f, true){
-			@Override
-			protected boolean removeEldestEntry(Map.Entry eldest) {
-				return size() > capacity;
-			}
-		};
+		head = new Node(0,0);
+		tail = new Node(0,0);
+		head.next = tail;
+		tail.pre = head;
+		this.cap = capacity;
 	}
 
 	public int get(int key) {
-		return map.getOrDefault(key, -1);
+		if(map.containsKey(key)){
+			Node node = map.get(key);
+			remove(node);
+			add(node);
+			return node.val;
+		}else return -1;
 	}
 
 	public void put(int key, int value) {
-		map.put(key, value);
+		if(map.containsKey(key)) remove(map.get(key));
+		if(map.size() == cap) remove(head.next);
+		add(new Node(key, value));
+	}
+	public void remove(Node node){
+		map.remove(node.key);
+		node.pre.next = node.next;
+		node.next.pre = node.pre;
+	}
+	public void add(Node node){
+		map.put(node.key, node);
+		Node p = tail.pre;
+		node.next = tail;
+		tail.pre = node;
+		node.pre = p;
+		p.next = node;
 	}
 }
