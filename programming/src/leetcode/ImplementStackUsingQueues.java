7bdff77066b0909e16a1b7ac1e8ac39c2048e3d7
“ee <1477261167@qq.com>
2019-11-19 10:23:17 +0800

    225.用队列实现栈

---
+++ programming/src/leetcode/ImplementStackUsingQueues.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueues {
+	Queue<Integer> queue1;
+	Queue<Integer> queue2;
+	public MyStack() {
+		queue1 = new LinkedList<>();
+		queue2 = new LinkedList<>();
+	}
+
+	/** Push element x onto stack. */
+	public void push(int x) {
+		queue1.add(x);
+		while(!queue2.isEmpty()) queue1.add(queue2.poll());
+		Queue<Integer> queue3 = new LinkedList<>();
+		queue3 = queue1;
+		queue1 = queue2;
+		queue2 = queue3;
+	}
+
+	/** Removes the element on top of the stack and returns that element. */
+	public int pop() {
+		return queue2.poll();
+	}
+
+	/** Get the top element. */
+	public int top() {
+		return queue2.peek();
+	}
+
+	/** Returns whether the stack is empty. */
+	public boolean empty() {
+		return queue2.isEmpty();
+	}
+}
