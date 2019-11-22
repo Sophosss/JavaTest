38989310272431ea88ff311f1bbb0f16885d079c
“ee <1477261167@qq.com>
2019-11-23 00:07:43 +0800

    大数相乘

--- programming/src/BigIntegerMul.java
+++ programming/src/BigIntegerMul.java
@@ -1,46 +1,37 @@
+import java.math.BigInteger;
+
 public class BigIntegerMul {
-	public static String go(String s1, String s2){
-		char sign1 = s1.charAt(0);
-		char sign2 = s2.charAt(0);
-		char sign = '+';
-		if(sign1 == '+' || sign1 == '-'){
-			sign = sign1;
-			s1 = s1.substring(1);
-		}
-		if(sign2 == '+' || sign2 == '-'){
-			if(sign == sign2) sign = '+';
-			else sign = '-';
-			s2 = s2.substring(1);
-		}
-		char [] c1 = new StringBuilder(s1).reverse().toString().toCharArray();
-		char [] c2 = new StringBuilder(s2).reverse().toString().toCharArray();
-		int len = s1.length() + s2.length();
-		int []nums = new int[len];
-		for (int i = 0; i < c1.length; i++) {
-			for (int j = 0; j < c2.length; j++){
-				int a1 = c1[i] - '0';
-				int b1 = c2[j] - '0';
-				nums[i + j] += a1 * b1;
+	public static String multiply(String num1, String num2) {
+		if(num1 == "" || num2 == "" || num1.length() == 0 || num2.length() == 0) return "0";
+		if(num1.equals("0") || num2.equals("0")) return "0";
+		int len1 = num1.length();
+		int len2 = num2.length();
+		char [] n1 = num1.toCharArray();
+		char [] n2 = num2.toCharArray();
+		int []nums = new int [len1+len2];
+		for(int i = 0; i < len1; i++){
+			for(int j = 0; j < len2; j++){
+				int a = n1[i] - '0';
+				int b = n2[j] - '0';
+				nums[i+j] += a * b;
 			}
 		}
-		for (int i = 0; i < nums.length; i++) {
-			if(nums[i] > 10){
-				nums[i+1] += nums[i] / 10;
-				nums[i] %= 10;
-			}
+		for(int i = nums.length - 1; i > 0; i--){
+			nums[i-1] += nums[i] / 10;
+			nums[i] = nums[i] % 10;
 		}
-		StringBuilder sb = new StringBuilder();
-		if(sign == '-') sb.append("-");
+		String s = "";
 		boolean flag = true;
-		for(int i = len-1; i >= 0; i--){
-			if(flag && nums[i] == 0) continue;
-			else flag = false;
-			sb.append(nums[i]);
+		for(int i = 0; i < nums.length - 1; i++){
+			s += "" + nums[i];
 		}
-		return sb.toString();
+		return s;
 	}
 
 	public static void main(String[] args) {
-		System.out.println(go("+11", "+11"));
+		System.out.println(multiply("5123123", "12123123"));
+		BigInteger b1 = new BigInteger("5123123");
+		BigInteger b2 = new BigInteger("12123123");
+		System.out.println(b1.multiply(b2));
 	}
 }
