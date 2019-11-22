fab57287833134f0772e9c38115f2a5954a56506
“ee <1477261167@qq.com>
2019-11-22 18:56:50 +0800

    448.找到所有数组中消失的数字

---
+++ programming/src/leetcode/FindAllNumbersDisappearedInArray.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllNumbersDisappearedInArray {
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		List<Integer> list = new ArrayList<>();
+		if(nums == null || nums.length == 0) return list;
+		for(int i = 0; i < nums.length; i++){
+			while(nums[i] != nums[nums[i] - 1]) swap(nums, i, nums[i] - 1);
+		}
+		for(int j = 0; j < nums.length; j++){
+			if(nums[j] != j + 1) list.add(j+1);
+		}
+		return list;
+	}
+	public void swap(int []nums, int i, int j){
+		nums[i] = nums[i] ^ nums[j];
+		nums[j] = nums[i] ^ nums[j];
+		nums[i] = nums[i] ^ nums[j];
+	}
+}
