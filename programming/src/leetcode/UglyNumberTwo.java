66e3c623483497032ee52569156312b06a1d1095
“ee <1477261167@qq.com>
2019-12-01 16:10:06 +0800

    264.丑数II

---
+++ programming/src/leetcode/UglyNumberTwo.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class UglyNumberTwo {
+	public static int nthUglyNumber(int n) {
+		int[] dp = new int[n];
+		dp[0] = 1;
+		int i2 = 0, i3 = 0, i5 = 0;
+		for (int i = 1; i < n; i++) {
+			int min = Math.min(dp[i2] * 2, Math.min(dp[i3] * 3, dp[i5] * 5));
+			if (min == dp[i2] * 2) i2++;
+			if (min == dp[i3] * 3) i3++;
+			if (min == dp[i5] * 5) i5++;
+			dp[i] = min;
+		}
+		return dp[n - 1];
+	}
+
+	public static void main(String[] args) {
+		UglyNumberTwo.nthUglyNumber(10);
+	}
+}
