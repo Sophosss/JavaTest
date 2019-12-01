60b3ea44c1aad9c26ecf4a41b1aca904c5a5be66
“ee <1477261167@qq.com>
2019-12-02 00:17:49 +0800

    10.正则表达式匹配

---
+++ programming/src/leetcode/RegularExpressionMatching.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class RegularExpressionMatching {
+	public boolean isMatch(String s, String p) {
+		int len1 = s.length();
+		int len2 = p.length();
+		boolean[][] dp = new boolean[len1+1][len2+1];
+		dp[0][0] = true;
+		for (int i = 2; i <= len2; i++) {
+			dp[0][i] = p.charAt(i-1) == '*' && dp[0][i-2];
+		}
+		for (int i = 1; i <= len1; i++) {
+			for (int j = 1; j <= len2; j++) {
+				if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1) == '.') dp[i][j] = dp[i-1][j-1];
+				if(p.charAt(j-1) == '*'){
+					if(s.charAt(i-1) != p.charAt(j-2) && p.charAt(j-2) != '.') dp[i][j] = dp[i][j-2];
+					else dp[i][j] = dp[i-1][j] || dp[i][j-1] || dp[i][j-2];
+				}
+			}
+		}
+		return dp[len1][len2];
+	}
+}
