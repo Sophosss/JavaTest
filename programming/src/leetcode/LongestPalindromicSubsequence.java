55fcdf4f9f3bf280f9ac2e220fd4291c76fe16c2
“ee <1477261167@qq.com>
2019-12-25 00:53:08 +0800

    516.最长回文子序列

---
+++ programming/src/leetcode/LongestPalindromicSubsequence.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class LongestPalindromicSubsequence {
+	public int longestPalindromeSubseq(String s) {
+		if(s == null || s.length() == 0) return 0;
+		int[][] dp = new int[s.length()][s.length()];
+		for (int i = 0; i < s.length(); i++) dp[i][i] = 1;
+		for (int i = s.length()-1; i >= 0; i--) {
+			for (int j = i+1; j < s.length(); j++) {
+				if(s.charAt(i) == s.charAt(j)) dp[i][j] = dp[i+1][j-1] + 2;
+				else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+			}
+		}
+		return dp[0][s.length()-1];
+	}
+}
