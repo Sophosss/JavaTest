b4157bb8253a2129d1bf3988f17297381a4e183d
“ee <1477261167@qq.com>
2019-12-29 12:34:32 +0800

    5296.两棵二叉搜索树中的所有元素

---
+++ programming/src/leetcodeWeekly/Two169.java
@@ -0,0 +1,23 @@
+package leetcodeWeekly;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Two169 {
+	public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+		List<Integer> list = new ArrayList<Integer>();
+		visit(root1, list);
+		visit(root2, list);
+		Collections.sort(list);
+		return list;
+	}
+
+	public void visit(TreeNode root, List<Integer> list) {
+		if (root!=null) {
+			list.add(root.val);
+			visit(root.left, list);
+			visit(root.right, list);
+		}
+	}
+}
